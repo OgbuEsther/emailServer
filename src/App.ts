b05c660233fd5fc3b2a42d23a7c06072f46cdf67
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-04-13 17:29:34 +0100

    working with ejs

--- src/App.ts
+++ src/App.ts
@@ -1,12 +1,19 @@
-import express, {Application} from "express"
-import cors from "cors"
-import user from "./router/userroutes"
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import user from "./router/userroutes";
+import ejs from "ejs"
 
-export const mainApp = async(app:Application) =>{
-    
-app.use(express.json())
-app.use(cors())
 
-app.use("/api" , user)
-}
 
+export const mainApp = async (app: Application) => {
+  app.set("view engine", "ejs");
+  app.use(express.json());
+app.use(express.static(`${__dirname} public/css`))
+app.use(express.static(`${__dirname} public/assets`))
+  app.use(cors());
+
+  app.use("/api", user);
+  app.get("/view", (req: Request, res: Response) => {
+    res.render("index");
+  });
+};
